b7df6c35c32801f7e4cf10d7c2bc646ffcd8c955
Zainab Lawal <zainabdamilolalawal@gmail.com>
2023-02-13 09:39:09 +0100

    feat:js-basics

---
+++ index.js
@@ -0,0 +1,12 @@
+let num = 2 * 4;
+console.log(num);
+
+var FirstName = "My Name is Zainab ";
+var LastName = "lawal";
+var StuternTrack = "I am in Frontend - development track";
+var Skills = "I love to code";
+var FavouriteColor = "My Favourite color is blue";
+var IndepenceYear = "Nigeria gained her independence in  1960";
+var sentence =
+  FirstName + LastName + StuternTrack + Skills + FavouriteColor + IndepenceYear;
+console.log(sentence);
